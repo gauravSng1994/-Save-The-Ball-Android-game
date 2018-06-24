16a59290e963237b50296be04bb74f5f1b889ac9
Gaurav Kumar <gauravsng1994@gmail.com>
2018-06-24 13:52:56 +0530

    first commit

---
+++ Assets/Scripts/Back.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Back : MonoBehaviour {
+    private float size1;
+    [SerializeField]
+    GameObject back;
+
+    Vector3 lastpos;
+
+	// Use this for initialization
+	void Start () {
+        size1 = back.transform.localScale.x;
+        lastpos = back.transform.position;
+        for(int x=0;x<50 ;x++ )
+        {
+            SpawnX();
+            SpawnZ();
+            
+        }
+		
+	}
+
+    // Update is called once per frame
+    private void SpawnX()
+    {
+        GameObject _back = Instantiate(back) as GameObject;
+        _back.transform.position = lastpos + new Vector3(size1, 0f, 0f);
+        lastpos = _back.transform.position;
+    }
+
+    private void SpawnZ()
+    {
+        GameObject _back = Instantiate(back) as GameObject;
+        _back.transform.position = lastpos + new Vector3(size1, 0f, 0f);
+        lastpos = _back.transform.position;
+    }
+}
